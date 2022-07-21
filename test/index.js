8a51f2967be214466d027fd9e33c32b6b2337763
dinookaswivel <dinooka@swivelgroup.com.au>
2022-07-21 13:26:24 +0530

    random name & email added

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 
 const supertest = require('supertest');
 const expect = require('chai').expect;
+const chance = require('chance').Chance();
 
 const request = supertest("https://airportgap.dev-tester.com/api/");
 const request2 = supertest("https://gorest.co.in/public/v2/");
@@ -47,17 +48,15 @@ describe('Airports', () => {
         await expect(res.body).to.not.be.empty;
     });
 
-    it.only('Test_04_POST create users', async () => {
+    it('Test_04_POST create users', async () => {
 
         const data = {
-            "name": "Shaun Marsh",
-            "email": "tim3.paine@test.com",
+            "name": chance.name(),
+            "email": chance.email({domain: "gmail.com"}),
             "gender": "male",
             "status": "active"
         }
         const res = await request2.post('users').set('Authorization','Bearer 1eaa7c6e1d432291b2a004e343c654ff3b37724a2cca3f81c8d4d997bd9d9187').send(data);
-
-
 
         console.log(res.body.data);
         console.log(res.status);
